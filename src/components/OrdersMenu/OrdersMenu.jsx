44e32c853779ba388d9ae637c8616f16d7372e16
satrain <mladen.trickovic5@gmail.com>
2025-02-17 14:26:33 +0100

    finish OrdersMenu component frontend

--- src/components/OrdersMenu/OrdersMenu.jsx
+++ src/components/OrdersMenu/OrdersMenu.jsx
@@ -4,10 +4,11 @@ const OrdersMenu = () => {
   return (
     <div className="orders-menu w-full py-1.5">
       <div className="menu w-full overflow-x-auto flex items-center gap-2.5">
-        <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">Sve</a>
         <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">U toku</a>
         <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">Čekaju u magacin</a>
-        <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">Čekaju u magacin</a>
+        <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">Sve</a>
+        <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">Završene</a>
+        <a className="item py-1 px-2.5 border border-[#494D59] rounded-[6px] shrink-0 bg-[#333740] hover:bg-[#2A2F39] text-[#6C727E] hover:text-[#F0F1F1]">Otkazane</a>
       </div>
     </div>
   )
