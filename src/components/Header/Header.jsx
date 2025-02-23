babdcec6440b172e0a9bb08ad91b71391b7b979a
satrain <mladen.trickovic5@gmail.com>
2025-02-23 01:09:44 +0100

    modify sidepane, make toggle button work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,70 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import SidePane from '../SidePane/SidePane'
-import IconButton from '../IconButton'
 
 import BurgerIcon from '../../assets/images/icons/burger-menu.svg'
 import BurgerIconActive from '../../assets/images/icons/burger-menu-active.svg'
 import ProfileIcon from '../../assets/images/icons/profile-icon.svg'
-import StoreIcon from '../../assets/images/icons/store-icon.svg'
-import SettingsIcon from '../../assets/images/icons/settings-icon.svg'
-import LogoutIcon from '../../assets/images/icons/logout-icon.svg'
 
 const Header = () => {
     const [paneContent, setPaneContent] = useState(null);
     const [isPaneOpen, setIsPaneOpen] = useState(false);
 
     const [isActive, setIsActive] = useState(false);
+
     const handleToggle = () => {
-        setIsActive((prevState) => !prevState);
+        setIsActive((prev) => !prev);
+        // here we should save courier's current active status in the db
     };
 
     const OpenMenu = () => {
-        setPaneContent(
-            <div>
-                <div className="active-status flex flex-col items-center mb-10 pb-10 border-b border-[#6C727E]">
-                    <div className="flex w-full items-center gap-2.5 mt-5 mb-2.5">
-                        <div className="w-full h-0.25 bg-[#6C727E]"></div>
-                        <p className="text-[#6C727E] font-medium shrink-0">Vaš status</p>
-                        <div className="w-full h-0.25 bg-[#6C727E]"></div>
-                    </div>
-                    <p className="text-center font-medium">
-                        {isActive ? 'Aktivan' : 'Neaktivan'}
-                    </p>
-                    <label className="relative inline-flex cursor-pointer items-center mt-2.5">
-                        <input 
-                        id="switch" 
-                        type="checkbox" 
-                        className="peer sr-only"
-                        checked={isActive}
-                        onChange={handleToggle}
-                         />
-                        <label for="switch" className="hidden"></label>
-                        <div className="peer h-7 w-13 rounded-full border bg-[#6C727E] after:absolute after:left-[2px] after:top-0.5 after:h-6 after:w-6 after:rounded-full after:border after:border-gray after:bg-white after:transition-all after:content-[''] peer-checked:bg-[#58A65C] peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:green"></div>
-                    </label>
-                </div>
-                <IconButton
-                title='Moje porudžbine'
-                icon={StoreIcon}
-                link='/'
-                />
-                <IconButton
-                title='Profil'
-                icon={ProfileIcon}
-                link='#'
-                />
-                <IconButton
-                title='Podešavanja naloga'
-                icon={SettingsIcon}
-                link='#'
-                />
-                <IconButton
-                title='Izloguj se'
-                icon={LogoutIcon}
-                link='#logout'
-                />
-            </div>
-        );
         setIsPaneOpen(true);
     };
 
@@ -86,11 +39,13 @@ const Header = () => {
             <a>Analitika</a>
             <a>Profil</a>
         </div>
-        <SidePane 
-        isOpen={isPaneOpen}
-        onClose={() => setIsPaneOpen(false)}
-        content={paneContent}
-        />
+        {isPaneOpen && (
+            <SidePane
+                isActive={isActive}
+                onToggle={handleToggle}
+                onClose={() => setIsPaneOpen(false)}
+            />
+        )}
     </>
   )
 }
--- src/components/SidePane/SidePane.jsx
+++ src/components/SidePane/SidePane.jsx
@@ -1,17 +1,68 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import CloseIcon from '../../assets/images/icons/close-icon.svg'
 
-const SidePane = ({ isOpen, onClose, content }) => {
+import IconButton from '../IconButton'
+import CloseIcon from '../../assets/images/icons/close-icon.svg'
+import StoreIcon from '../../assets/images/icons/store-icon.svg'
+import SettingsIcon from '../../assets/images/icons/settings-icon.svg'
+import LogoutIcon from '../../assets/images/icons/logout-icon.svg'
+import ProfileIcon from '../../assets/images/icons/profile-icon.svg'
+
+const SidePane = ({ isActive, onToggle, onClose }) => {
   return (
     <motion.div 
     initial={{ x: '-100%' }}
-    animate={{ x: isOpen ? '0' : '-100%' }}
+    animate={{ x: 0 }}
+    exit={{ x: '-100%' }}
     transition={{ type: 'spring', stiffness: 300, damping: 30 }}
     className="fixed bottom-0 left-0 w-full h-[92.6%] z-4 bg-[#2A2F39] text-[#F0F1F1] p-4 overflow-y-auto"
     >
         <div>
-            {content}
+          <div className="active-status flex flex-col items-center mb-10 pb-10 border-b border-[#6C727E]">
+              <div className="flex w-full items-center gap-2.5 mt-5 mb-2.5">
+                  <div className="w-full h-0.25 bg-[#6C727E]"></div>
+                  <p className="text-[#6C727E] font-medium shrink-0">Moj status</p>
+                  <div className="w-full h-0.25 bg-[#6C727E]"></div>
+              </div>
+              <label className="relative inline-flex cursor-pointer items-center mt-2.5">
+                  <input 
+                      id="switch" 
+                      type="checkbox" 
+                      className="peer sr-only"
+                      checked={isActive}
+                      onChange={onToggle}
+                  />
+                  <div className="relative h-7 w-14 rounded-full border bg-[#6C727E] transition-all 
+                      after:absolute after:left-[2px] after:top-0.5 after:h-6 after:w-6 after:rounded-full 
+                      after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] 
+                      peer-checked:bg-[#58A65C] peer-checked:after:translate-x-7 peer-checked:after:border-white">
+                  </div>
+              </label>
+
+              <p className="text-center font-medium mt-2">
+                  {isActive ? 'Aktivan' : 'Neaktivan'}
+              </p>
+          </div>
+          <IconButton
+          title='Moje porudžbine'
+          icon={StoreIcon}
+          link='/'
+          />
+          <IconButton
+          title='Profil'
+          icon={ProfileIcon}
+          link='#'
+          />
+          <IconButton
+          title='Podešavanja naloga'
+          icon={SettingsIcon}
+          link='#'
+          />
+          <IconButton
+          title='Izloguj se'
+          icon={LogoutIcon}
+          link='#logout'
+          />
         </div>
     </motion.div>
   )
